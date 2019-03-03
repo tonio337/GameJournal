96b75d3f7ce7adf1704c0f81b978173676e33df9
Antonio Lee <tonoi337@gmail.com>
2019-03-03 16:27:04 -0600

    Prior to today, made a bare-bones skeleton UI in Android app. Added types for Game and Journal (with internal Entry).  Started to populate sample data (manually, could not get SampleData Android Tools function to work).
    Today, expanded Journal functionality the littlest bit and expanded Unit test (63% coverage as of today). Installed and configured Git local repository. Pushed out to remote repository on Github. (Also made prior personal repoistories on Github private.)
    
    Established API key for IDGB game database. Intend to use IDGB for game data lookup rather than storing it manually.
    
    Finally, will likely use this file's notes to base the commit message, until I'm more comfortable with Git commits.

---
+++ src/test/java/com/tonio337/gamejournal/GameJournalUnitTest.kt
@@ -0,0 +1,24 @@
+package com.tonio337.gamejournal
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class GameJournalUnitTest {
+
+    @Test
+    fun game_isCreated() {
+        assertEquals("Super Mario Bros. 2", Game("Super Mario Bros. 2").name)
+    }
+
+    @Test
+    fun journal_getFuns(){
+        // games and journal are defined in SampleData
+        assertEquals(games, journal.games)
+    }
+}
